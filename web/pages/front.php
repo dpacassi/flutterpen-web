5ed416bee71a9d3d6273d2ff04d1faed9c8c5a1e
David Pacassi Torrico <dpacassi@gmail.com>
2019-06-01 14:53:29 +0200

    Finalize the first front page draft

--- web/pages/front.php
+++ web/pages/front.php
@@ -5,8 +5,10 @@
         <h1><span class="flutter">Flutter</span><span class="pen">Pen</span></h1>
         <h2>Your <span class="flutter">Flutter</span> Developer Community</h2>
 
-        <a class="button--primary" href="/">Sign Up</a>
-        <a class="button--secondary" href="/">Create a FlutterPen</a>
+        <footer>
+          <a class="button button--primary" href="/">Sign Up</a>
+          <a class="button button--secondary" href="/">Create a FlutterPen</a>
+        </footer>
       </article>
     </section>
   </div>
